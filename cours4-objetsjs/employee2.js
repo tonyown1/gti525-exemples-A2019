0baf58e8470b5d9615db03b87d379f2777378877
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2019-09-09 18:13:22 -0400

    Exemples de code pour le cours 4 -- objets js

---
+++ cours4-objetsjs/employee2.js
@@ -0,0 +1,33 @@
+// Création d'objets avec la syntaxe Object.create
+
+var Person = {
+	print: function() {
+		console.log("Person " + this.firstName + " " + this.lastName + " " + this.gender );
+	}
+};
+
+var p = Object.create(Person);
+p.firstName = "Linda";
+p.lastName = "James";
+p.gender = "female";
+
+p.print();
+console.log(p);
+console.log( Object.getPrototypeOf(p) );
+console.log( p.prototype );	
+
+var e = Object.create(Person, {
+		firstName: {value: "John"},
+		lastName: {value: "Smith"},
+		gender: {value: "Male"}
+	}
+);
+e.title = "manager";
+e.print = function() {
+		console.log("Employee " + this.title);
+		Person.print.call(this); 
+};
+
+e.print();
+console.log(e);
+console.log( Object.getPrototypeOf(e) );	
