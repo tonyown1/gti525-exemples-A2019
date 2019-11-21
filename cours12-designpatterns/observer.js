e35e5fa86558e7c11e81083df940fc930a4b4ecc
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2019-11-21 12:26:23 -0500

    observer solution

--- cours12-designpatterns/observer.js
+++ cours12-designpatterns/observer.js
@@ -1,3 +1,5 @@
+// Solution to the class activity -- observer design pattern
+
 function setupObservers() {
   /*var txts = document.getElementsByTagName("input");
   for (var i=0; i<txts.length; i++) {
@@ -37,7 +39,7 @@ window.onload = function() {
 
   var btn = document.getElementById("mybutton");
   extend(btn, new Subject());
-  // Notifier lorsque clic
+  // Notify when clicked
   btn.onclick = function() { btn.notify("World!"); }
 
   setupObservers();
